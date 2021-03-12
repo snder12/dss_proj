55e7fac085dab1ae0f01afc44743946e7169ef85
snder12nr@gmail.com <snder12nr@gmail.com>
2021-03-12 13:48:55 +0700

    Third changes

---
+++ src/test.js
@@ -0,0 +1,23 @@
+var budi = "budiono";
+
+console.log(budi);
+
+var angka = 5;
+
+console.log(angka);
+
+var bol = Boolean(angka < 7);
+
+console.log(bol);
+
+var jumlah = budi + angka;
+
+console.log(jumlah);
+
+var jumlah2 = angka + 300;
+
+console.log(jumlah2);
+
+var jumlah3 = angka + "300";
+
+console.log(jumlah3);
